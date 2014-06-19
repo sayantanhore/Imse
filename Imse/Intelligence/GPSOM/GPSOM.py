23d6e176bfc3cd6dc7f41d3043f87a0bf6ec3354
Lasse Tyrväinen <sundokan@gmail.com>
2014-06-19 14:47:39 +0300

    Completed the initial changes before trying to use it with the gui

--- Imse/Intelligence/GPSOM/GPSOM.py
+++ Imse/Intelligence/GPSOM/GPSOM.py
@@ -13,58 +13,40 @@ class GPSOM(object):
     
     def __init__(self, images_number_iteration, images_number_total, category):
         self.image_features = np.asfarray(np.load("../../data/cl25000.npy"), dtype="float32")
-        self.shown_images_mask = np.ones(self.image_features.shape[0], dtype=bool)
-        self.setsize = images_number_iteration
+        self.first_sample_size = images_number_iteration
         self.images_number = images_number_total
-        self.first_sample_size = (int(math.ceil(math.sqrt(math.sqrt(self.images_number)))))**2
-        self.images_shown = []
-        self.previous_images = []
+        self.feedback_indices = []
         self.feedback = []
-        self.iteration = 0
+        self.iteration = 0  # TODO: use this to change the exploration/exploitation ratio
         self.gp = None
         self.selected_images = []
     
     def FirstRound(self):
         """Pre-processing stage - sample first set of images
         Take random images"""
-        self.images_shown = np.random.choice(self.images_number, self.first_sample_size, replace=False)
-        for idx in self.images_shown:
+        self.feedback_indices = np.random.choice(self.images_number, self.first_sample_size, replace=False)
+        for idx in self.feedback_indices:
             self.shown_images_mask[idx] = True
-        self.previous_images = self.images_shown
         self.iteration += 1
-        return self.images_shown
+        return self.feedback_indices
     
-    def Predict(self, feedback, data):
+    def Predict(self, feedback, num_predictions):
         self.feedback = self.feedback + feedback
         if not self.gp:
             self.gp = gp_cuda.GaussianProcessGPU(self.image_features,
                                                  self.feedback,
-                                                 self.images_shown)  # TODO: where do we get the indices and features?
+                                                 self.feedback_indices)
         # What this method returns
         images = []
         # Copy all the values that will be used as they have to be modified only within iteration
         # Current training set with images and feedback and clusters assignments
         ucb, mean = self.gp.gaussianProcess()
-
-
-        chosen_image_idx = ucb[self.shown_images_mask].argmax()
-        if chosen_image not in images_shown:
-            if chosen_image not in self.selected_images:
-                images.append(chosen_image)
-                # To sample next images we add fake feedback
-                images_shown.append(chosen_image)
-                feedback.append(mean[index_chosen_image])
-        # Delete the chosen image from the current copy of cluster_to_datapoints in order not to sample it again
-        clusters[chosen_model_vector] = numpy.delete(clusters[chosen_model_vector],index_chosen_image)
-        # if we have deleted all datapoints from the cluster, delete the cluster
-        if len(clusters[chosen_model_vector])==0:
-            del clusters[chosen_model_vector]
-            print chosen_model_vector+self.images_number
-            index_chosen_model_vector = list(clusters_names).index(chosen_model_vector+self.images_number)
-            print index_chosen_model_vector
-            clusters_names = numpy.delete(clusters_names, index_chosen_model_vector)
-        self.images_shown = self.images_shown + images
+        if num_predictions == 1:
+            chosen_image_indices = [ucb.argmax()]
+        else:
+            chosen_image_indices = sorted(ucb)[-num_predictions:]
+        self.feedback_indices = self.feedback_indices + chosen_image_indices
         self.iteration += 1
-        self.previouse_images = images
-        return images
-        #return choosen_clusters
+        return self.feedback_indices
+
+
