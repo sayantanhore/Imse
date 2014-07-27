44714c357df4615cad76c1c20172f5afeb3ce8cd
Sayantan Hore <sayantan.hore.86@gmail.com>
2014-07-27 19:07:00 +0300

    Fluid design first commit, colour pallete first row designed

---
+++ static/js/imse.js
@@ -0,0 +1,18 @@
+// JS script for IMSE
+
+// Author: Sayantan Hore
+// Created: 01.06.2014
+
+"use strict";
+
+function createFrontPageDisplayBoxes(){
+
+    var sqBox = $("<div class = 'sqbox'></div>");
+    return sqBox;
+    
+}
+
+$(document).ready(function(){
+    //$("body").append(createFrontPageDisplayBoxes());
+});
+
