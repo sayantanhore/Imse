ccdf7491284cc03574053fbc31cb9c59f58e54d4
Sayantan Hore <sayantan.hore.86@gmail.com>
2014-08-01 04:13:52 +0300

    Border shadow added on color palette

--- static/js/color_palette.js
+++ static/js/color_palette.js
@@ -12,60 +12,10 @@ function createColorHyperlink(){
     return $("<a href></a>")
 }
 
-function createColorHyperlinkParentDiv(){
-    //var link = createColorHyperlink();
-    
-    return $("<div class='col-xs-1 col-md-1'></div>"); 
-}
 
-function createMultipleColorHyperlinkParentDivContainer(){
-    //var link = createColorHyperlink();
-    
-    return $("<div class='col-xs-5 col-md-5'></div>"); 
-}
-
-function createColorPaletteFragment(noOfContainers, linkVisible){
-    if(noOfContainers === 1){
-        var link = createColorHyperlink();
-        if (linkVisible === true){
-            link.addClass("thumbnail");
-        }
-        var linkParentDiv = createColorHyperlinkParentDiv();
-        linkParentDiv.append(link);
-        return createColorHyperlinkParentDiv().append(linkParentDiv);
-    }
-    else{
-        var multipleLinkParentDiv = createMultipleColorHyperlinkParentDivContainer();
-        for (var i = 0; i < noOfContainers; i++){
-            var link = createColorHyperlink();
-            link.addClass("thumbnail");
-            var linkParentDiv = createColorHyperlinkParentDiv();
-            linkParentDiv.append(link);
-            multipleLinkParentDiv.append(linkParentDiv);
-        }
-        return multipleLinkParentDiv;
-    } 
-}
-
-function colorPaletteRow_2(){
+function colorRow(){
     var row = $("<div class = 'row'>");
-    row.append(createColorPaletteFragment(1, false));
-    row.append(createColorPaletteFragment(12, true));
-    row.append(createColorPaletteFragment(1, true));
-    row.append(createColorPaletteFragment(12, true));
-    return row;
-}
-
-function createFrontPageDisplayBoxes(){
-
-    //var sqBox = $("<div class = 'sqbox'></div>");
-    //return sqBox;
-   
-}
-
-function colorPaletteRow(){
-    var row = $("<div class = 'row'>");
-    var col = $("<div class='col-xs-12 col-md-12 color-container'></div>");
+    var col = $("<div class='col-xs-12 col-md-12 color-box'></div>");
     for (var colourCounter = 0; colourCounter < 25; colourCounter ++){
         col.append(createColorHyperlink().addClass("thumbnail"));
     }
@@ -78,10 +28,11 @@ function colorPaletteRow(){
 $(document).ready(function(){
     
     //$("body").append(createFrontPageDisplayBoxes());
+    var colorPalette = $(".color-palette")
     for (var rowCounter = 0; rowCounter < 15; rowCounter++){
-        $(".container-fluid").append(colorPaletteRow());
+        colorPalette.append(colorRow());
     }
-    $(".container-fluid").append($("<div class = 'page-header'></div>"));
+    //$(".container-fluid").append($("<div class = 'page-header'></div>"));
     //$(".container-fluid").append($("<button type='button' class='btn btn-default btn-sm pull-right'>Proceed</button>"));
     console.log("First colour")
     console.log(colours[0])
