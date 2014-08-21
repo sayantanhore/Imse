8d8ef514e4ac2f7de7cb47736d7098ddba507846
Sayantan Hore <sayantan.hore.86@gmail.com>
2014-08-22 01:47:37 +0300

    Images shown without square box

---
+++ static/js/image-box.js
@@ -0,0 +1,47 @@
+// ImageBox component
+
+// Author Sayantan Hore
+// Created on 19.08.2014
+
+// --------------------------------
+
+function ImageBox(){
+    this.outerContainer = null;
+    this.imageBoxArray = [];
+}
+ImageBox.prototype.createOuterContainer = function(){
+    this.outerContainer = $("<div></div>")
+    this.outerContainer.addClass("outer-image-container");
+    $("body").append(this.outerContainer);
+    
+}
+ImageBox.prototype.createImageBox = function(){
+    if (this.createOuterContainer === null){
+        this.createOuterContainer();
+    }
+    var imageBox = $("<div></div>");
+    imageBox.addClass("thumbnail");
+    
+    //imageBox.css("width", parseFloat(this.outerContainer.width() / 4 - 5)).css("height", parseFloat(this.outerContainer.width() / 4 - 5));
+    var closeButton = $("<div>x</div>");
+    closeButton.addClass("btn-close");
+    
+    //imageBox.append(closeButton);
+    this.imageBoxArray.push(imageBox);
+    return imageBox;
+}
+
+ImageBox.prototype.loadImage = function(imPath, imageBox){
+    var img = $("<img></img>")
+    img.attr("src", imPath);
+    imageBox.append(img);
+
+    img.load(function(){
+        //console.log($(this).height());
+        imgWidth = $(this).width();
+        $(this).parent().height(rowHeight);
+        $(this).height(rowHeight);
+        //console.log($(this).width());
+        $(this).parent().width($(this).width());
+    });
+}
